be41e931e6a230ddbc01a21ff055c4e36a9e15fe
“bryan” <bryan.dov.bergman@gmail.com>
2020-11-21 17:01:46 +0200

    made the code a bit neater and worked out a bug

--- app/src/main/java/com/example/alert_dialogs_/MainActivity.java
+++ app/src/main/java/com/example/alert_dialogs_/MainActivity.java
@@ -13,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
     TextView myTextView;
-    String myLangPref = "language setting = ";
+    final static String LNG_BOOL = "language bool";
+    String myLangPrefIntro = "language preference = ";
+    String myLangPref = "";
+    boolean langPrefSet = false;
     final static String ENGLISH = "English";
     final static String SPANISH = "Spanish";
     final static String LANG_KEY = "language";
@@ -23,13 +26,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myTextView = (TextView)findViewById(R.id.myTextView);
+        myTextView = (TextView) findViewById(R.id.myTextView);
         sharedPreferences = this.getSharedPreferences("com.example.memorable_places_app", Context.MODE_PRIVATE);
-        myLangPref += sharedPreferences.getString(LANG_KEY, "no set language preference found");
-        myTextView.setText(myLangPref);
+        langPrefSet = sharedPreferences.getBoolean(LNG_BOOL, false);
+        updatePreferences();
+        callDialogBuilder();
+    }
 
+    private void callDialogBuilder() {
         new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setIcon(android.R.drawable.ic_btn_speak_now)
                 .setTitle("Language Preferences")
                 .setMessage("Choose Which Language To Use?")
                 .setPositiveButton("English", new DialogInterface.OnClickListener() {
@@ -37,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this, "English!", Toast.LENGTH_SHORT).show();
                         sharedPreferences.edit().putString(LANG_KEY, "english").apply();
+                        sharedPreferences.edit().putBoolean(LNG_BOOL, true);
+                        updatePreferences();
                     }
                 })
                 .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
@@ -44,9 +52,25 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this, "Spanish!", Toast.LENGTH_SHORT).show();
                         sharedPreferences.edit().putString(LANG_KEY, "spanish").apply();
+                        sharedPreferences.edit().putBoolean(LNG_BOOL, true);
+                        updatePreferences();
 
                     }
                 })
                 .show();
+    }
+
+    private void updatePreferences() {
+        myLangPref = myLangPrefIntro;
+
+        if(!langPrefSet) {
+            myLangPref += sharedPreferences.getString(LANG_KEY, myLangPrefIntro + "no set language preference.");
+            myTextView.setText(myLangPref);
+            langPrefSet = true;
+        }else{
+            myLangPref += sharedPreferences.getString(LANG_KEY, "no set language preference found");
+            myTextView.setText(myLangPref);
+        }
+
     }
 }
