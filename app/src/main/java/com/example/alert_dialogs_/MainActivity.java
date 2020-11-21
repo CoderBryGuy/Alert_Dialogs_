8803514f4f88f70997b79457caeda47cd65ac0d9
“bryan” <bryan.dov.bergman@gmail.com>
2020-11-21 18:11:57 +0200

    added a menu

--- app/src/main/java/com/example/alert_dialogs_/MainActivity.java
+++ app/src/main/java/com/example/alert_dialogs_/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.alert_dialogs_;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,17 +25,45 @@ public class MainActivity extends AppCompatActivity {
     final static String ENGLISH = "English";
     final static String SPANISH = "Spanish";
     final static String LANG_KEY = "language";
+    final static String NO_LNG_SET = "no set language preference.";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.english:
+                updatePreferences(ENGLISH);
+                return true;
+            case R.id.spanish:
+                updatePreferences(SPANISH);
+                return true;
+            default:
+                return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         myTextView = (TextView) findViewById(R.id.myTextView);
         sharedPreferences = this.getSharedPreferences("com.example.memorable_places_app", Context.MODE_PRIVATE);
         langPrefSet = sharedPreferences.getBoolean(LNG_BOOL, false);
         updatePreferences();
-        callDialogBuilder();
+//        callDialogBuilder();
+        Log.i("BOOL", "  " +  langPrefSet);
+        if(!langPrefSet){
+            callDialogBuilder();
+        }
     }
 
     private void callDialogBuilder() {
@@ -42,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this, "English!", Toast.LENGTH_SHORT).show();
-                        sharedPreferences.edit().putString(LANG_KEY, "english").apply();
+                        sharedPreferences.edit().putString(LANG_KEY, ENGLISH).apply();
                         sharedPreferences.edit().putBoolean(LNG_BOOL, true);
                         updatePreferences();
                     }
@@ -51,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this, "Spanish!", Toast.LENGTH_SHORT).show();
-                        sharedPreferences.edit().putString(LANG_KEY, "spanish").apply();
+                        sharedPreferences.edit().putString(LANG_KEY, SPANISH).apply();
                         sharedPreferences.edit().putBoolean(LNG_BOOL, true);
                         updatePreferences();
 
@@ -62,15 +95,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void updatePreferences() {
         myLangPref = myLangPrefIntro;
-
         if(!langPrefSet) {
-            myLangPref += sharedPreferences.getString(LANG_KEY, myLangPrefIntro + "no set language preference.");
+            myLangPref += sharedPreferences.getString(LANG_KEY,  NO_LNG_SET);
             myTextView.setText(myLangPref);
             langPrefSet = true;
         }else{
-            myLangPref += sharedPreferences.getString(LANG_KEY, "no set language preference found");
+            myLangPref += sharedPreferences.getString(LANG_KEY, NO_LNG_SET);
             myTextView.setText(myLangPref);
         }
+
+    }
+
+    private void updatePreferences(String lng) {
+        myLangPref = myLangPrefIntro;
+        if(!langPrefSet) {
+            myLangPref += lng;
+            myTextView.setText(myLangPref);
+            langPrefSet = true;
+        }else{
+            myLangPref += lng;
+            myTextView.setText(myLangPref);
+        }
+        sharedPreferences.edit().putString(LANG_KEY, lng).apply();
 
     }
 }
